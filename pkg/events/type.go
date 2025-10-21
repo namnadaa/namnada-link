2f6484f60d7aa5fc15bd430241bfbe52aef5030a
Alexander Nesterov <looss016@gmail.com>
2025-10-21 20:19:55 +0300

    add Fetcher and Processor interfaces with basic event types

---
+++ pkg/events/type.go
@@ -0,0 +1,25 @@
+package events
+
+// Fetcher is an interface for fetching a batch of events from an external source.
+type Fetcher interface {
+	Fetch(limit int) ([]Event, error)
+}
+
+// Processor is an interface for processing a single event.
+type Processor interface {
+	Process(e Event) error
+}
+
+// Type represents the type of an event.
+type Type int
+
+const (
+	Unknown Type = iota
+	Message
+)
+
+// Event represents a single event in the system, such as a user message.
+type Event struct {
+	Type Type
+	Text string
+}
